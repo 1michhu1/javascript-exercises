551edc28aca9fb509913adba2e4af2a63554f3dd
1michhu1 <michh2005@gmail.com>
2023-08-07 00:27:56 -0400

    complete exercises 10-12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    // arr = []
+    // for (let i = 0; i < books.length; i++) {
+    //     arr.push(books[i].title)
+    // }
+    // return arr
+    return books.map(book => book.title)
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+// carly 28, ray 49, jane 29
 
+    sortedArr = people.sort( (a,b) => {
+        if
+        if ((+(a.yearOfDeath) - +(a.yearOfBirth)) > (+(b.yearOfDeath) - +(b.yearOfBirth))) {
+            console.log(a.name, " is greater than ", b.name)
+            console.log((+(a.yearOfDeath) - +(a.yearOfBirth)) > (+(b.yearOfDeath) - +(b.yearOfBirth)))
+            return 1
+        } else {
+            console.log(a.name, " is less than ", b.name)
+
+            return -1
+        }
+    })
+    console.log(sortedArr)
+    return sortedArr[people.length - 1]
 };
 
 // Do not edit below this line
